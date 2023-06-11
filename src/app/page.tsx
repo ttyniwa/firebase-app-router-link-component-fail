e9fdc76157b2f226fd331224a3056a19ba31801a
ttyniwa <noencrypt@gmail.com>
2023-06-11 17:10:54 +0900

    Static exportsの設定を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/select-company">select-company</Link>
+      <Link href="/select-company">link to select-company</Link>
     </div>
   );
 }
